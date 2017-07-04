2abd2706982481c7ab15f2a485f0d59e94e63a8d
Oricul <Oricul@users.noreply.github.com>
2017-07-03 20:19:26 -0500

    Remove debug, attempt to tag author.

--- twitchy.py
+++ twitchy.py
@@ -147,12 +147,11 @@ class Twitch():
                     for row in cur1:
                         if row[0].lower() == username.lower() and str(row[3]) == str(ctx.message.server.id):
                             sqladdby = ctx.message.server.get_member(str(row[2]))
-                            print("{0} ||| {1}".format(sqladdby,row[2]))
                             if sqladdby == None:
                                 sqladdby = 'NULL'
                             whenadd = datetime.datetime.strftime(row[1],"%a, %b %d, %Y %I:%M:%S %p")
                             outMSG = discord.Embed(colour=discord.Colour(0xFFFF00))
-                            outMSG.set_author(name="{0} was already added by {1} on {2} (US/EST).".format(tStatus[1]['display_name'],sqladdby,whenadd))
+                            outMSG.set_author(name="{0} was already added by <@{1}> on {2} (US/EST).".format(tStatus[1]['display_name'],row[2],whenadd))
                             await self.bot.send_message(discord.Object(id=ctx.message.channel.id),embed=outMSG)
                             sqldb1.close()
                             return
