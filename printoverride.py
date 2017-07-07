b3ca9ba9d23c5318aa0e938d4405fd67dafb2511
Oricul <Oricul@users.noreply.github.com>
2017-07-07 15:23:01 -0500

    Decided I preferred the 24-hour clock.

--- printoverride.py
+++ printoverride.py
@@ -5,7 +5,7 @@ oldPrintFunc = print
 
 def print(text, flush=True, **kwargs):
     if '\n' in text:
-        oldPrintFunc("{0}>>\n{1}".format(datetime.datetime.strftime(datetime.datetime.now(), "%m/%d/%Y %I:%M:%S"), text), flush=flush, **kwargs)
+        oldPrintFunc("{0}>>\n{1}".format(datetime.datetime.strftime(datetime.datetime.now(), "%m/%d/%Y %H:%M:%S"), text), flush=flush, **kwargs)
     else:
-        oldPrintFunc("{0}>> {1}".format(datetime.datetime.strftime(datetime.datetime.now(),"%m/%d/%Y %I:%M:%S"),text), flush=flush, **kwargs)
+        oldPrintFunc("{0}>> {1}".format(datetime.datetime.strftime(datetime.datetime.now(),"%m/%d/%Y %H:%M:%S"),text), flush=flush, **kwargs)
     return
