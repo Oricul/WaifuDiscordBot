cdeb838f87723a1f00416bed5e1c3579693604fd
Oricul <Oricul@users.noreply.github.com>
2017-07-07 15:18:14 -0500

    Formatting for time in printoverride.

--- printoverride.py
+++ printoverride.py
@@ -4,5 +4,8 @@ import datetime
 oldPrintFunc = print
 
 def print(text, flush=True, **kwargs):
-    oldPrintFunc("{0}>> {1}".format(datetime.datetime.strftime(datetime.datetime.now(),"%m/%d/%Y %I:%M:%S"),text), flush=flush, **kwargs)
+    if '\n' in text:
+        oldPrintFunc("{0}>>\n{1}".format(datetime.datetime.strftime(datetime.datetime.now(), "%m/%d/%Y %I:%M:%S"), text), flush=flush, **kwargs)
+    else:
+        oldPrintFunc("{0}>> {1}".format(datetime.datetime.strftime(datetime.datetime.now(),"%m/%d/%Y %I:%M:%S"),text), flush=flush, **kwargs)
     return
